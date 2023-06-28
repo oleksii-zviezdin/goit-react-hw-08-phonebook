1321ad872ca637c949140671538551cd37217247
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-06-28 20:13:19 +0200

    remove null from fallback

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
 import { Suspense } from 'react';
+import { Loader } from './Loader/Loader';
 
 export const Layout = () => {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
       <AppBar />
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </div>
